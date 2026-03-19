2ce374b5208e43b65c06940353d8c599422b4390
Deborah Florea <deborahflorea97@gmail.com>
2026-03-19 12:20:40 +0200

    Add bill functionality on driver page (just UI)

--- src/main/java/com/example/parkingsystem/service/UserServiceI.java
+++ src/main/java/com/example/parkingsystem/service/UserServiceI.java
@@ -5,4 +5,6 @@ import com.example.parkingsystem.model.User;
 public interface UserServiceI {
     boolean usernameTaken(String username);
     void createUser(User user);
+    float getMoneySpent(String username);
+    User getUser(String username);
 }
